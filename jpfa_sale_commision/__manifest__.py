de742c7fa70043a05a6bc811bd9cf14de96ba533
alvinadji766-odoo <alvinadji766@gmail.com>
2026-09-22 05:22:23 +0700

    Scaffold Module

---
+++ jpfa_sale_commision/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Japfa - Sale Commision',
+    'version': '18.0.1.0.0',
+    'category': 'Uncategorized',
+    'summary': '',
+    'description': """
+Long description of the module's purpose
+========================================
+
+""",
+    'author': 'Japfa Odoo Dev',
+    'website': 'https://odoo.com',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'demo': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
